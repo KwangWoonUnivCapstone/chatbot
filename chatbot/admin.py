b44ac4c2f420d08602a06e2079b89037de015505
jsjhyun <gusrnl98@naver.com>
2024-03-22 19:35:03 +0900

    feat: 기본 model 등록 및 admin 설정

--- chatbot/admin.py
+++ chatbot/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import LawyerChatbot
 
 # Register your models here.
+
+@admin.register(LawyerChatbot)
+class LawyerChatbotAdmin(admin.ModelAdmin):
+    list_display = ('user_input', 'bot_response', 'timestamp')
+
--- chatbot/models.py
+++ chatbot/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class LawyerChatbot(models.Model):
+    user_input = models.TextField()  # 사용자 입력
+    bot_response = models.TextField()  # 챗봇 응답
+    timestamp = models.DateTimeField(auto_now_add=True)  # 타임스탬프
+    def __str__(self):
+        return f"User: {self.user_input} - Bot: {self.bot_response}"
